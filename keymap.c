032d5ed4fbfbb409ea2f2fc9bcba41e8cfbaff44
Wen-Long Yang <matesea@gmail.com>
2024-07-17 23:00:34 +0800

    add leader key config

--- keymap.c
+++ keymap.c
@@ -8,25 +8,27 @@ enum custom_keycodes {
   ST_MACRO_0,
 };
 
-
+#define _QWERTY 0
+#define _NAVI 1
+#define _SYS 2
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_voyager(
+  [_QWERTY] = LAYOUT_voyager(
     KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,               KC_8,           KC_9,           KC_0,           KC_MINUS,
-    KC_GRV,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,               KC_I,           KC_O,           KC_P,           KC_BSLS,
+    KC_GRV,       KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,               KC_I,           KC_O,           KC_P,           KC_BSLS,
     KC_TAB,         KC_A,           KC_S,           MT(MOD_LALT, KC_D),MT(MOD_LSFT, KC_F),KC_G,                                     KC_H,           MT(MOD_RSFT, KC_J), MT(MOD_LALT, KC_K),KC_L,        KC_SCLN,        KC_QUOTE,
     KC_CAPS,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,               KC_COMMA,       KC_DOT,         KC_SLASH,       KC_EQUAL,
-                                                    MT(MOD_LGUI, KC_ENTER),KC_LEFT_CTRL,                                   LT(1,KC_BSPC),  KC_SPACE
+                                                    MT(MOD_LGUI, KC_ENTER),KC_LEFT_CTRL,                                   LT(_NAVI,KC_BSPC),  KC_SPACE
   ),
-  [1] = LAYOUT_voyager(
-    TO(0),          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_PSCR,        KC_SCRL,
+  [_NAVI] = LAYOUT_voyager(
+    TO(_QWERTY),          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                           KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_PSCR,        KC_SCRL,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_HOME,        KC_PGDN,        KC_PAGE_UP,     KC_END,         KC_INSERT,      KC_TRANSPARENT,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_DELETE,      KC_TRANSPARENT,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_LBRC,        KC_RBRC,        KC_LCBR,        KC_RCBR,        KC_APPLICATION, KC_TRANSPARENT,
                                                     KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT
   ),
-  [2] = LAYOUT_voyager(
-    TO(0),          KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,                                          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+  [_SYS] = LAYOUT_voyager(
+    TO(_QWERTY),          KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,                                    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     QK_BOOT,        KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,                                         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, KC_F11,         KC_F12,         RGB_VAD,        RGB_VAI,        RGB_TOG,                                        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     QK_DYNAMIC_TAPPING_TERM_PRINT,QK_DYNAMIC_TAPPING_TERM_DOWN,QK_DYNAMIC_TAPPING_TERM_UP,KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP,KC_AUDIO_MUTE,                                  KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
@@ -42,9 +44,9 @@ const uint16_t PROGMEM combo4[] = { MT(MOD_RSFT, KC_J), MT(MOD_LALT, KC_K), COMB
 
 combo_t key_combos[COMBO_COUNT] = {
     COMBO(combo0, LSFT(KC_LEFT_CTRL)),
-    COMBO(combo1, TO(1)),
+    COMBO(combo1, TO(_NAVI)),
     COMBO(combo2, ST_MACRO_0),
-    COMBO(combo3, TO(2)),
+    COMBO(combo3, TO(_SYS)),
     COMBO(combo4, KC_ESCAPE),
 };
 
@@ -134,15 +136,16 @@ bool achordion_chord(uint16_t tap_hold_keycode,
 
 uint16_t achordion_timeout(uint16_t tap_hold_keycode) {
     tap_hold_keycode &= 0xff;
+    // only enable achordion for homerow
     switch (tap_hold_keycode) {
-        // bypass achordion timeout for thumb keys
-        case KC_ENTER:
-        case KC_BSPC:
-        /* case MT(MOD_LGUI, KC_ENTER): */
-        /* case LT(1,KC_BSPC): */
-            return 0;
+        case KC_D:
+        case KC_F:
+        case KC_J:
+        case KC_K:
+            return 500;
     }
-    return 500; // default 1000, recommend 800
+    // bypass achordion timeout
+    return 0;
 }
 #endif
 
@@ -172,7 +175,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const key_override_t **key_overrides = (const key_override_t *[]){
     // SHIFT+BSPC -> DEL
     // &ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL),
-    &ko_make_basic(MOD_MASK_SHIFT, LT(1,KC_BSPC), KC_DEL),
+    &ko_make_basic(MOD_MASK_SHIFT, LT(_NAVI,KC_BSPC), KC_DEL),
 
     // SHIFT+ESC -> ~
     &ko_make_basic(MOD_MASK_SHIFT, KC_ESC, S(KC_GRV)),
@@ -195,17 +198,20 @@ void leader_start_user(void) {
 void leader_end_user(void) {
     if (leader_sequence_one_key(KC_S)) {
         // Leader, sp => space
-        tap_code16(KC_SPACE);
-    } else if (leader_sequence_one_key(KC_E)) {
-        tap_code16(KC_ESC);
+        tap_code(KC_SPACE);
     } else if (leader_sequence_one_key(KC_1)) {
-        tap_code16(TO(1));
+        layer_move(_NAVI);
     } else if (leader_sequence_one_key(KC_2)) {
-        tap_code16(TO(2));
-    } else if (leader_sequence_one_key(KC_C)) {
-        tap_code16(LSFT(KC_LEFT_CTRL));
-    } else if (leader_sequence_two_keys(KC_T, KC_C)) {
-      SEND_STRING(SS_LCTL(SS_TAP(X_A)) SS_DELAY(100) SS_TAP(X_LBRC));
+        layer_move(_SYS);
     }
+
+    /*
+       if (leader_sequence_one_key(KC_C))
+         tap_code16(LSFT(KC_LEFT_CTRL));
+       if (leader_sequence_two_keys(KC_T, KC_C))
+        SEND_STRING(SS_LCTL(SS_TAP(X_A)) SS_DELAY(100) SS_TAP(X_LBRC));
+       if (leader_sequence_one_key(KC_E))
+        tap_code16(KC_ESC);
+     */
 }
 #endif
