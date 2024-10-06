0072f7769221851c88a40b3ae7fcce99ab34460e
Wen-Long Yang <matesea@gmail.com>
2024-10-06 12:21:13 +0800

    remove IME from corner as combo

--- keymap.c
+++ keymap.c
@@ -56,13 +56,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_ESC,   KC_1,   KC_2,   KC_3,   KC_4,     KC_5,
             KC_EQL,   KC_Q,   KC_W,   KC_E,   KC_R,     KC_T,
             KC_TAB,   BASE_A, BASE_S, BASE_D, BASE_F,   KC_G,
-            IME,      BASE_Z, BASE_X, BASE_C, BASE_V,   KC_B,
+            KC_COLN,  BASE_Z, BASE_X, BASE_C, BASE_V,   KC_B,
                                               BASE_ENT, BASE_UNDS,
 
                             KC_6,    KC_7,   KC_8,      KC_9,     KC_0,      KC_MINS,
                             KC_Y,    KC_U,   KC_I,      KC_O,     KC_P,      KC_BSLS,
                             KC_H,    BASE_J, BASE_K,    BASE_L,   BASE_SCLN, KC_QUOT,
-                            KC_N,    BASE_M, BASE_COMM, BASE_DOT, BASE_SLSH, KC_COLN,
+                            KC_N,    BASE_M, BASE_COMM, BASE_DOT, BASE_SLSH, KC_DQUO,
                             KC_BSPC, KC_SPC
             ),
 
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             _______, _______, _______, _______, _______, _______,
             _______, _______, _______, _______, _______, _______,
             _______, _______, _______, _______, _______, _______,
-            IME,     _______, _______, _______, _______, _______,
+            _______, _______, _______, _______, _______, _______,
                                                 _______, _______,
 
                               _______, _______, _______, _______, _______,  _______,
